b958274a85ea0657f5580eb0a559aaf414f172d9
Vik Williamson <williamson.vik@gmail.com>
2023-05-01 20:28:35 -0700

    Add ErrorBoundary; Update packages; Update metadata

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,25 +1,23 @@
-import React from 'react';
+import React from "react";
 
 //===Components===
-import Card from './Card';
+import Card from "./Card";
 
-const CardList = ({robots}) => {
-    return (
+const CardList = ({ robots }) => {
+  return (
     <div>
-        {
-            robots.map((user, i) => {
-                return (
-                    <Card 
-                        id={robots[i].id} 
-                        key={`robotUser${user.id}`} 
-                        name={robots[i].name} 
-                        email={robots[i].email} 
-                    />
-                );
-            })
-        }
+      {robots.map((user, i) => {
+        return (
+          <Card
+            id={robots[i].id}
+            key={`robotUser${user.id}`}
+            name={robots[i].name}
+            email={robots[i].email}
+          />
+        );
+      })}
     </div>
-    );
-}
+  );
+};
 
 export default CardList;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,53 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 //===Components===
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
+import CardList from "../components/CardList";
+import ErrorBoundary from "../components/ErrorBoundary";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
 
 //===Styles===
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchField: ''
-        }
-    }
-
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(res => {
-                return res.json();
-            })
-            .then(users => {
-                this.setState({ robots: users })
-            })
-    }
-
-    onSearchChange = (event) => {
-        this.setState({ searchField: event.target.value })
-    }
-
-    render() {
-        const { robots, searchField } = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchField.toLowerCase())
-        });
-
-        return (!robots.length) ?  <h1>Loading</h1> :
-         (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <Scroll>
-                    <CardList robots={filteredRobots} />
-                </Scroll>
-            </div>
-        );      
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchField: "",
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+    const { robots, searchField } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
+        </Scroll>
+      </div>
+    );
+  }
 }
 
 export default App;
